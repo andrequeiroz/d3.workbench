3ad87a5eac920bcaf3b085236cbc87193f5f73f1
André Queiroz <chegadetentar@gmail.com>
2018-11-21 15:33:34 -0200

    botões de paineis

--- src/painel.js
+++ src/painel.js
@@ -2,10 +2,12 @@ function executar(dados) {
 
   d3.json(dados).then(function(x) {
 
+    // painel principal
     var painel = d3.select("body")
                    .append("div")
                    .attr("id", "painel");
 
+    // seletor de provas
     painel.append("p")
           .text("Prova: ")
           .append("select")
@@ -20,6 +22,19 @@ function executar(dados) {
     var prova = x.filter(function(el) {
       return el.codigo === seletor.options[seletor.selectedIndex].value;
     });
+
+    // menu de opções
+    var paineis = [ "Masculino", "Feminino" ];
+    var menu = painel.append("div")
+                     .attr("id", "painel-menu");
+
+    var botoes = menu.selectAll("button")
+                     .data(paineis)
+                     .enter()
+                     .append("button")
+                     .attr("class", "painel-botoes")
+                     .text(function(d) { return d; })
+
     prova = prova[0];
     console.log(prova);
   });
